4cfc4503ee0f59249274364b16c7044b842a3ebb
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2020-04-27 15:24:04 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get ('/', 'PageController@home');
+Route::get ('/features', 'PageController@features');
+
+Route::get ('/asset', 'AssetController@index');
