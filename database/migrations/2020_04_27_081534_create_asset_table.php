9db24a3b5b477a1a16f9726ca70556353736d2ac
Muhammad Zaahid <muhammadzaahid24@gmail.com>
2020-04-27 15:18:22 +0700

    add migration

---
+++ database/migrations/2020_04_27_081534_create_asset_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asset', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');            
+            $table->char('code', 10)->unique();
+            $table->string('vendor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asset');
+    }
+}
